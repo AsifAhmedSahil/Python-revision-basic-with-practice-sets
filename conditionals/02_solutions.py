066f98a9877c61c21a94f2f3e7c9ed7af6a2e578
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-11-03 23:19:45 +0600

    update

--- conditionals/02_solutions.py
+++ conditionals/02_solutions.py
@@ -1,10 +1,10 @@
-age = 26
-day = "Wednesday"
+# age = 26
+# day = "Wednesday"
 
-price = 12 if age >= 18 else 8
+# price = 12 if age >= 18 else 8
 
-if day == "Wednesday":
-    # price = price - 2
-    price -= 2
+# if day == "Wednesday":
+#     # price = price - 2
+#     price -= 2
 
-print("Ticket price for you is $",price)
+# print("Ticket price for you is $",price)
